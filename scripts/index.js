428ec179718cf8590270ecb7b266b532b8b8c18f
Murzin Ivan <murzin.1910@yandex.ru>
2024-11-01 17:24:16 +0300

    project

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,25 @@
 // @todo: Темплейт карточки
+const places = document.querySelector('.places__list')
+const cardTemplate = document.querySelector('#card-template').content;
+initialCards.forEach(function (element) {
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__image').src = element.link
+    cardElement.querySelector('.card__title').textContent = element.name
+  
+    
+    places.append(cardElement)
+    
+  });
+document.querySelector('.page').onclick = function(e){
+    if(e.target.className !== 'card__delete-button')return
+    const item = e.target.closest('.card')
+    item.remove()
+}
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+
+//@todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
 
