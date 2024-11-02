71ef4b42d03eb5b434249f3c4ced12b888b22bb7
Murzin Ivan <murzin.1910@yandex.ru>
2024-11-02 16:01:25 +0300

    commite

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,23 @@
-// @todo: Темплейт карточки
-const places = document.querySelector('.places__list')
+const places = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
-initialCards.forEach(function (element) {
-    const cardElement = cardTemplate.cloneNode(true);
-    cardElement.querySelector('.card__image').src = element.link
-    cardElement.querySelector('.card__title').textContent = element.name
-  
-    
-    places.append(cardElement)
-    
-  });
-document.querySelector('.page').onclick = function(e){
-    if(e.target.className !== 'card__delete-button')return
-    const item = e.target.closest('.card')
-    item.remove()
+
+function createCard(data, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = data.link;
+  cardElement.querySelector('.card__title').textContent = data.name;
+  cardElement.querySelector('.card__image').alt = data.name;
+
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+  return cardElement;
 }
 
-// @todo: DOM узлы
+function handleDeleteCard(element) {
+  element.remove();
+}
 
-
-//@todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(data => {
+  const cardElement = createCard(data, handleDeleteCard);
+  places.append(cardElement);
+});
